be322f682dcdef144c9d815097b0559371b3b48f
eden <edenv30@gmail.com>
2021-01-19 19:06:36 +0200

    optimized & fix duration & comeback empty list

--- functions.py
+++ functions.py
@@ -17,7 +17,7 @@ def parse_url(self):
     path = urlparse.urlparse(url).path
     params = urlparse.parse_qs(urlparse.urlparse(url).query)
     return path, params
-#check if have enough slaves available and return the availables slaves
+#check if have enough slaves available and return the available slaves
 def available_slaves(amount, slaves):
     cnt = 0
     a_slaves = []
@@ -33,18 +33,23 @@ def update_duration_slaves(slaves_dictionary, duration, amount):
     slaves = []
     for slave in slaves_dictionary:
         if slave["duration"] == 0:
-            slave["duration"] = duration
+            slave["duration"] = duration + 1
+            # slave["unit-test"] = time.time() # unit test
             slaves.append(slave["ip"])
         if len(slaves) == amount: return slaves_dictionary, slaves
 #retuan the duration time that the client needs to come back again
 def come_back(slaves_dictionary, amount, duration):
     slaves = []
     my_list = sorted(slaves_dictionary, key=lambda k: k['duration'])
-
     for slave in my_list:
         if slave["duration"] <= duration and slave["duration"] != 0:
             slaves.append(slave)
         if len(slaves) == amount:
             break
-#   check if slaves empty
     return slaves[amount-1]['duration']
+# send response to the client
+def send_response_(self, status_code, content_type, msg):
+    self.send_response(status_code)
+    self.send_header('Content-Type', content_type)
+    self.end_headers()
+    self.wfile.write(bytes(msg, "utf8"))
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from functions import slaves_assign, parse_url, available_slaves, update_duration_slaves, come_back
+from functions import slaves_assign, parse_url, available_slaves, update_duration_slaves, come_back, send_response_
 import time, json, threading, sys
+import logging
 
 slaves_dictionary = slaves_assign()
 callback_collection = []
@@ -11,6 +12,8 @@ def lifecycle():
         time.sleep(1)
         for slave in slaves_dictionary:
             if slave["duration"] != 0:
+                # sub = math.floor(time.time() - slave["unit-test"])
+                # print(sub)
                 slave["duration"] -= 1
 
 t = threading.Thread(target=lifecycle)
@@ -28,45 +31,38 @@ class MyHandler(BaseHTTPRequestHandler):
                 if amount > 10:
                     msg = '[!] Error: You must insert amount less or equal to 10\n'
                     print(msg)
-                    self.send_response(400)
-                    self.send_header('Content-Type', 'application/json')
-                    self.end_headers()
-                    self.wfile.write(bytes(msg, "utf8"))
+                    send_response_(self, 400, 'text/plain', msg)
                 available, slaves = available_slaves(amount, slaves_dictionary)
 
                 if not available:
-    #               count duration
                     new_amount = amount - len(slaves)
                     slaves_come_back = come_back(slaves_dictionary, new_amount, duration)
-                    self.send_response(200)
-                    self.send_header('Content-Type', 'application/json')
-                    self.end_headers()
-                    self.wfile.write(bytes(json.dumps({"slaves": slaves, "come_back": slaves_come_back }), "utf8"))
+                    msg = json.dumps({"slaves": slaves, "come_back": slaves_come_back })
+                    send_response_(self, 200, 'application/json', msg)
                     return
                 else:
                     slaves_dictionary, slaves = update_duration_slaves(slaves_dictionary, duration, amount)
-                    self.send_response(200)
-                    self.send_header('Content-Type', 'application/json')
-                    self.end_headers()
-                    self.wfile.write(bytes(json.dumps({"slaves": slaves}), "utf8"))
+                    msg = json.dumps({"slaves": slaves})
+                    send_response_(self, 200, 'application/json', msg)
                     return
             except Exception as error:
                 msg = '[!] Error: {0}\n You must check your request'.format(error)
                 print(msg)
-                self.send_response(400)
-                self.send_header('Content-Type', 'application/json')
-                self.end_headers()
-                self.wfile.write(bytes(msg, "utf8"))
+                send_response_(self, 400, 'text/plain', msg)
 
 if __name__ == '__main__':
     port = 8080
     args_list = sys.argv
     if args_list:
         try:
-            port = int(args_list[2])
-        except:
+            if args_list[1] == "--port" and args_list[2].isnumeric() :
+                port = int(args_list[2])
+            else:
+                print('[!] Incorrect command, we use an default port')
+        except Exception as e:
             print('[!] Incorrect command, we use an default port')
             print('[!] Error: Example run command: python server.py --port 8080')
+            logging.exception(e)
     print('starting server on port {0}...'.format(port))
 
     server_address = ('localhost', port)
